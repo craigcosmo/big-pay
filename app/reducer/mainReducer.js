8a149541ef0db27a3a59407ec24f6eb452065905
Craig Nguyen <craigcosmo@gmail.com>
2018-07-02 01:01:49 +0700

    updatw

--- app/reducer/mainReducer.js
+++ app/reducer/mainReducer.js
@@ -2,7 +2,6 @@ import {combineReducers} from 'redux'
 import {routerReducer} from 'react-router-redux'
 import homeReducer from 'homeReducer'
 
-
 export default combineReducers({
 	home: homeReducer,
 	routing: routerReducer
